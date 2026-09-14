63f9549fc230285c9400608bb1a827a52b3cc750
SeuNome <SeuEmail>
2026-09-14 08:52:16 -0300

    modified:   .env #	new file:   app.js #	new file:   src/config/database.js

---
+++ src/config/database.js
@@ -0,0 +1,14 @@
+const {Sequelize}=require("sequelize")
+require("dotenv").config()
+
+const sequelize=Sequelize(
+process.env.DB_NAME,
+process.env.DB_USER,
+process.env.DB_PASSWORD,{
+    host:process.env.DB_HOST,
+    port:process.env.DB_HOSTdb_port,
+    dialect:process.env.DB_CONNECTION ||"mysql"
+}
+)
+
+isModuleNamespaceObject.exports=sequelize;
